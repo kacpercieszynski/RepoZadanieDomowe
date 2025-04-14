af82ecf9a6f777d4b85e52481b1cade90c78d02a
Kacper Cieszynski <kacpercieszynski@mat.umk.pl>
2025-04-14 20:57:22 +0200

    polecenie 3

--- projekt.cpp
+++ projekt.cpp
@@ -2,19 +2,47 @@
 
 using namespace std;
 
+// Funkcja obliczająca silnię liczby całkowitej
+unsigned long long factorial(int n) {
+    if (n < 0) {
+        cout << "Silnia nie jest zdefiniowana dla liczb ujemnych." << endl;
+        return 0;
+    }
+    unsigned long long wynik = 1;
+    for (int i = 2; i <= n; ++i) {
+        wynik *= i;
+    }
+    return wynik;
+}
+
 int main() {
     int a, b;
-    cout << "Podaj liczbe calkowita ";
+    cout << "Podaj dwie liczby calkowite: ";
     cin >> a >> b;
+
     int wyjscie;
     do {
-        cout << endl;
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+
+        switch (wyjscie) {
+            case 1:
+                cout << "Silnia z " << a << " wynosi: " << factorial(a) << endl;
+                break;
+            case 2:
+                cout << "Silnia z " << b << " wynosi: " << factorial(b) << endl;
+                break;
+            case 0:
+                cout << "Koniec programu." << endl;
+                break;
+            default:
+                cout << "Nieprawidlowy wybor. Sprobuj ponownie." << endl;
+        }
     } while(wyjscie != 0);
+
     return 0;
 }
 
